d2d2b3217001b3dde877f5e9cd786ca0b087128e
Hussaini Usman <hussainmaina27@gmail.com>
2024-08-31 16:06:20 +0100

    update projects page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,28 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Engausa Academy',
+    description: `Engausa Academy is a multilingual e-learning platform (mobile & web) I single handedly built from scratch for an education institution that offers skills development courses in areas such as Solar Energy System Installation, Electric Fencing, CCTV Camera Installation & Maintenance, Electric Wiring, Graphic Design, Animation and more. I still maintain this project.`,
+    imgSrc: '/static/images/engausa-academy.jpeg',
+    href: 'https://www.engausaacademy.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Sultan Film Factory',
+    description: `Sultan Film Factory is a movie streaming app I built for an entertainment company that is based in Nigeria, and they needed a way to bring their films to a larger audience worldwide.`,
+    imgSrc: '/static/images/sultan.jpg',
+    href: 'https://play.google.com/store/apps/details?id=com.sultanfilmfactory.mobile',
+  },
+  {
+    title: 'Drf-Logto',
+    description: `Integration for Logto Authentication with Django Rest Framework. Has support for JWT based authentication. Currently under development, so use with caution.`,
+    imgSrc: '/static/images/logto.jpg',
+    href: 'https://github.com/housain-maina/drf-logto',
   },
 ]
 
